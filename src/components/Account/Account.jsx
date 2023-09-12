bed44a5f2cdcff8b7775dd97b4d3b954d8994364
ClementVaugoyeau <clement.vaugoyeau@gmail.com>
2023-09-12 18:18:03 +0200

    fix tailwind className

--- src/components/Account/Account.jsx
+++ src/components/Account/Account.jsx
@@ -7,10 +7,10 @@ import MenuSignUp from "../MenuSignUp/MenuSignUp";
 
 const Account = () => {
   return (
-    <div class="flex min-h-full flex-col  justify-center px-6 py-12 lg:px-8">
+    <div className="flex min-h-full flex-col  justify-center px-6 py-12 lg:px-8">
       <MenuSignUp/>
 
-      <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
         <Formik>
           <form className="space-y-6" action="#" method="POST">
             <div>
@@ -19,7 +19,7 @@ const Account = () => {
                 className="block text-sm font-medium leading-6 text-gray-900">
                 Adresse email
               </label>
-              <div class="mt-2">
+              <div className="mt-2">
                 <input
                   id="email"
                   name="email"
@@ -32,28 +32,28 @@ const Account = () => {
             </div>
 
             <div>
-              <div class="flex items-center justify-between">
+              <div className="flex items-center justify-between">
                 <label
-                  Htmlfor="password"
-                  class="block text-sm font-medium leading-6 text-gray-900">
+                  htmlfor="password"
+                  className="block text-sm font-medium leading-6 text-gray-900">
                   Mot de passe
                 </label>
-                <div class="text-sm">
+                <div className="text-sm">
                   <a
                     href="#"
-                    class="font-semibold text-indigo-600 hover:text-indigo-500">
+                    className="font-semibold text-indigo-600 hover:text-indigo-500">
                     Mot de passe oublié ?
                   </a>
                 </div>
               </div>
-              <div class="mt-2">
+              <div className="mt-2">
                 <input
                   id="password"
                   name="password"
                   type="password"
                 
                   required
-                  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
@@ -61,7 +61,7 @@ const Account = () => {
             <div>
               <button
                 type="submit"
-                class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
                 Me connecter
               </button>
             </div>
